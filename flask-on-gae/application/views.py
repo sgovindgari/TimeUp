720af429261e1525a040187bd2b78b3720492abf
Ruoran Wang <ruoranwang@gmail.com>
2013-08-03 22:59:52 -0700

    session works

--- flask-on-gae/application/views.py
+++ flask-on-gae/application/views.py
@@ -1,5 +1,5 @@
 from application import app
-from flask import render_template, request, flash, redirect, url_for, session , jsonify
+from flask import render_template, request, flash, redirect, url_for, session , jsonify, g
 from google.appengine.ext import db
 from google.appengine.api import urlfetch
 from models import *
@@ -8,48 +8,89 @@ import geopy, geopy.distance
 import time, random, urllib, hashlib, json
 import facebook
 
+from functools import wraps
+from flask import request
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if "user" in session:
+            return redirect(url_for('login', next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
+
+# if 'username' in session:
+#             return 'Logged in as %s' % escape(session['username'])
+#             return 'You are not logged in'
+
+@login_required
 @app.route('/loggedin')
 def index():
     return render_template('index.html')
 
-    # Return the html for login
+# Return the html for login
+
 @app.route('/')
 def login():
     return render_template('login.html')
-    
-    # Handle the actual Login Post request
+
+# Handle the actual Login Post request
+@login_required
 @app.route('/login', methods=["POST"])
 def loginPost():
     uid = request.form['uid']
     token = request.form['token']
+
     if uid and token:
         try:
             graph = facebook.GraphAPI(token)
-	    profile = graph.get_object("me")
-	    name = profile["name"]
-	    newUser = User(id = int(uid), username = name)
-	    newUser.save()
-	except facebook.GraphAPIError:
-	    name = "token Error"
-	if  name == "token Error":
-	    data = name
-	else:
-	    data = "success"
+            profile = graph.get_object("me")
+
+            # check if this user exists in datastore
+            q = User.all().filter('id =', uid)
+            user_count = q.count()
+            if user_count == 1:
+                session['user'] = q.get()
+            else:
+                name = profile["name"]
+                newUser = User(id = int(uid), username = name)
+                newUser.save()
+                session['user'] = newUser
+        except facebook.GraphAPIError:
+            name = "token Error"
+    else:
+        name = "argument Error"
+
+    if  name == "token Error" or name == "argument Error":
+        data = name
+    else:
+        data = "success"
     return data
-    
+
+@login_required
 @app.route('/tasks/new', methods=["POST"])
 def newTask():
     desc = request.form['description']
     duration = int(request.form['duration'])
 
     newTask = Task(description = desc, duration = duration)
-    key = newTask.save();
+    key = newTask.save()
+
+    app.logger.debug("added %s   %s  %s" % (key, desc, type(key)))
+    user = session['user'] 
+    user.tasks.insert(0, key)
+    user.save()
+    session['user'] = user
 
     data = {"status": "ok"}
     return jsonify(data)
 
+@login_required
 @app.route('/tasks', methods=["GET"])
 def allTask():
-    q = Task.all()
-    task_list = [{"description": task.description, "duration": task.duration} for task in q.run(limit=15)]
+    app.logger.debug(session['user'].tasks)
+    tasks = db.get(session['user'].tasks[:15])
+
+    task_list = [{"description": task.description, "duration": task.duration} for task in tasks]
     return jsonify({"task_list": task_list})
+     
