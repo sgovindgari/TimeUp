fd6b9eeb34099b22e84ce3ba97dae0af038fb0a9
Spandana Govindgari <sgovindgari@gmail.com>
2013-08-03 21:34:39 -0700

    Added user to the db on login

--- flask-on-gae/application/models.py
+++ flask-on-gae/application/models.py
@@ -4,10 +4,7 @@ class User(db.Model):
     # this id is facebook id
     id = db.IntegerProperty(required = True)
     username = db.StringProperty(required = True)
-    email = db.EmailProperty()
-    passwd = db.StringProperty()
     tasks = db.ListProperty(db.Key)
-    friends = db.ListProperty(long)
 
 class Task(db.Model):
     description = db.StringProperty(required = True)  
--- flask-on-gae/application/views.py
+++ flask-on-gae/application/views.py
@@ -8,43 +8,35 @@ import geopy, geopy.distance
 import time, random, urllib, hashlib, json
 import facebook
 
-
 @app.route('/loggedin')
 def index():
     return render_template('index.html')
 
-# Return the html for login
-
+    # Return the html for login
 @app.route('/')
 def login():
     return render_template('login.html')
-
-# Handle the actual Login Post request
+    
+    # Handle the actual Login Post request
 @app.route('/login', methods=["POST"])
 def loginPost():
     uid = request.form['uid']
     token = request.form['token']
-
     if uid and token:
         try:
             graph = facebook.GraphAPI(token)
-            profile = graph.get_object("me") 
-            name = profile["name"]
-        except GraphAPIError:
-            name = "token Error"
-
+	    profile = graph.get_object("me")
+	    name = profile["name"]
+	    newUser = User(id = int(uid), username = name)
+	    newUser.save()
+	except facebook.GraphAPIError:
+	    name = "token Error"
     return render_template('test.html', data = name)
-
+    
 @app.route('/tasks/new', methods=["POST"])
 def newTask():
     desc = request.form['description']
-    duration = int(request.form['duration'])
-
-
+    duration = int(request.form['duration'])    
     newTask = Task(description = desc, duration = duration)
-    key = newTask.save();
-
+    key = newTask.save();    
     return render_template('test.html', data = key)
-
-
-
